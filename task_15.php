b71d91865be630f5c682290fdfa2a69c15e3b900
dioge <cherkashn@gmail.com>
2018-07-12 14:22:41 +0300

    Add files via upload

---
+++ task_15.php
@@ -0,0 +1,26 @@
+<pre>
+
+<?php
+
+	$n = rand();
+	// $n = 100;
+
+	echo ("The number is: ".$n."<br>");
+	
+	for($i = 2; $i <= ($n/2); $i++){
+
+		if($n%$i == 0){
+
+			$divisors[] = $i;
+			continue;
+		}
+	}
+
+	if($divisors){
+		
+		var_dump ($divisors);
+	}else{
+
+		echo ("The number has no divisors");
+	}
+?>
