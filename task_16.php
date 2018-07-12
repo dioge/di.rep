ccd0766f70791d4dd98c34887ce23ef9a7bb2273
dioge <cherkashn@gmail.com>
2018-07-12 17:44:15 +0300

    Update task_16.php

--- task_16.php
+++ task_16.php
@@ -1,35 +1,25 @@
 <pre>
 
 <?php
-
 	$n = rand(2, 10000);
-
-	echo ("The number is: ".$n."<br>");
+	echo ("The range is from 2 to ".$n.". Perfect numbers are: <br>");
 	
 	for($i = 2; $i < $n; $i++){
-
 		for($j = 1; $j <= ($i/2); $j++){
-
 			if($i%$j == 0){
-
 				$divisors[] = $j;
 			}
 		}
-
 		$count = count($divisors);
 		
 		$sum = 0;
 		
 		for($j = 0; $j < $count; $j++){
-
 			$sum += $divisors[$j];
 		}
-
 		if($sum == $i){
-
 			$perfect_numbers[] = $i;
 		}
-
 		unset($divisors);
 	}
 	
