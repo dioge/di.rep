33693b531adda56833e0dcd12e2b9c5ce70c0ea2
dioge <cherkashn@gmail.com>
2018-07-12 16:13:09 +0300

    Add files via upload

---
+++ task_16.php
@@ -0,0 +1,37 @@
+<pre>
+
+<?php
+
+	$n = rand(2, 10000);
+
+	echo ("The number is: ".$n."<br>");
+	
+	for($i = 2; $i < $n; $i++){
+
+		for($j = 1; $j <= ($i/2); $j++){
+
+			if($i%$j == 0){
+
+				$divisors[] = $j;
+			}
+		}
+
+		$count = count($divisors);
+		
+		$sum = 0;
+		
+		for($j = 0; $j < $count; $j++){
+
+			$sum += $divisors[$j];
+		}
+
+		if($sum == $i){
+
+			$perfect_numbers[] = $i;
+		}
+
+		unset($divisors);
+	}
+	
+	var_dump ($perfect_numbers);
+?>
