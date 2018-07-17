210bd310be3e77d8eb308ff875b52c847598e5ce
dioge <cherkashn@gmail.com>
2018-07-17 18:00:48 +0300

    Update task_8.php

--- task_8.php
+++ task_8.php
@@ -2,45 +2,24 @@
 
 <?php
 
-	// Сохраняем в массив $i_array числа, разбитые на цифры
-	
-	for($i = 1000; $i <= 9999; $i++){
+	for($i = 1000; $i < 10000; $i++){
 		
-		$i .= '';
-		$length = strlen($i);
-		
-		for($j = 0; $j < $length; $j++){
+		for($j = $i; $j >= 1; $j = (int)($j/10)){
 			
-			$j_array[$j] = $i[$j];
-		}
+			$j_digit = $j%10;
 		
-		$i_array[] = $j_array;
-	}
-	
-	// Проверка на повторение цифр
-	
-	$count = count($i_array);
-	
-	for($i = 0; $i < $count; $i++){
-		
-		$number_array = $i_array[$i];
-		
-		for($j = 0; $j < $length; $j++){
+			for($k = (int)($j/10); $k >= 1; $k = (int)($k/10)){
+				
+				$k_digit = $k%10;
 
-			if($j == ($length - 1)){
-			
-				$numbers[] = $number_array;
-			}
-			
-			for($k = ($j + 1); $k < $length; $k++){
-
-				if($number_array[$j] == $number_array[$k]){
+				if($j_digit == $k_digit){
 					
-					break 2;
+					continue 3;
 				}
 			}
 		}
-	}
+		
+		echo ($i."<br>");
+    }
 	
-	var_dump ($numbers);
 ?>
