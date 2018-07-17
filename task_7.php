28df01f66c21392f3e2acdd4d4409b5f081e3c51
dioge <cherkashn@gmail.com>
2018-07-17 17:40:00 +0300

    Update task_7.php

--- task_7.php
+++ task_7.php
@@ -2,37 +2,25 @@
 
 <?php
 
-	// Сохраняем в массив цифры, составляющие число
-
-	$n = (string) mt_rand();
-
-	var_dump($n);
+	echo ($n = mt_rand()."<br>");
 	
-	$length = strlen($n);
+	for($i = $n; $i >= 1; $i = (int)($i/10)){
+
+		$i_digit = $i%10;
 	
-	for($i = 0; $i < $length; $i++){
+		for($j = (int)($i/10); $j >= 1; $j = (int)($j/10)){
+			
+			$j_digit = $j%10;
 
-		$n_array[] = $n[$i];
-	}
-	
-	// Проверка на повторение цифр
-
-	for($i = 0; $i < $length; $i++){
-
-		if($i == ($length - 1)){
-		
-			echo ("The number ".$n." has no doubling digits");
+			if($i_digit == $j_digit){
+				
+				echo ("The number has following doubling digits: ".$i_digit."<br>");
+				
+				break 2;
+			}
 		}
 		
-			for($j = ($i + 1); $j < $length; $j++){
-
-				if($n_array[$i] == $n_array[$j]){
-					
-					echo ("The number ".$n." has doubling digits: ".$n_array[$i]);
-					break 2;
-				}
-			}
-		
-		
 	}
+
 ?>
+
