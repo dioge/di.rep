163429cd0b8237b2e46300280d8b88f85812ce57
dioge <cherkashn@gmail.com>
2018-07-17 16:45:50 +0300

    Update task_6.php

--- task_6.php
+++ task_6.php
@@ -2,58 +2,23 @@
 
 <?php
 
-	// Сохраняем в массив $i_array массивы чисел, разбитых на цифры
-	
-	for($i = 1000; $i <= 9999; $i++){
+	for($i = 1000; $i < 10000; $i++){
 		
-		$i .= '';
-		$length = strlen($i);
+		$n = $i;
 		
-		for($j = 0; $j < $length; $j++){
+		while($n >= 1){
 			
-			$j_array[$j] = $i[$j];
+			$digit = $n%10;
+			
+			if($digit == 1 || $digit == 4 || $digit == 5 || $digit == 6 || $digit == 8 || $digit == 9){
+
+				continue 2;
+			}
+			
+			$n = (int)($n/10);
 		}
-		
-		$i_array[] = $j_array;
-	}
-
-	// Проверка
 	
-	$i_count = count($i_array);
-
-	for($i = 0; $i < $i_count; $i++){
-		
-		for($j = 0; $j <= $length; $j++){
-            
-			$digit = (int)$i_array[$i][$j];
-			
-			if($digit === 0 || $digit === 2 || $digit === 3 || $digit === 7){
-			
-				if($j == (count($i_array[$i]) - 1)){
-				
-					$numbers[] = $i_array[$i];
-				}
-            }else{
-				
-                break;
-            }
-        }
-    }    
-
-	//Объединяем цифры в числа для удобного вывода
-	$number_count = count($numbers);
-
-	for($i = 0; $i < $number_count; $i++){
-		
-		$j_number = '';
-		
-		for($j = 0; $j < $length; $j++){
-			
-			$j_number .= $numbers[$i][$j];
-		}
-		
-		$joint_numbers[] = $j_number;
-	}
+		echo ($i."<br>");
+    }
 	
-	var_dump($joint_numbers);
 ?>
