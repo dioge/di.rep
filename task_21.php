481c3115b9cfc97ad6f577ef63f162220dfe27e3
dioge <cherkashn@gmail.com>
2018-07-18 17:16:18 +0300

    Update task_21.php

--- task_21.php
+++ task_21.php
@@ -1,68 +1,37 @@
-<pre>
-
 <?php
 
 	$n = rand();
 
 	$m = rand();
 
-	echo ("The numbers are ".$n." and ".$m."<br>");
-	
-	
-	// Делители n
-	
-	for($i = 1; $i <= ($n/2); $i++){
-
-		if($n%$i == 0){
-
-			$n_divisors[] = $i;
-		}
-	}
-
-	$n_divisors[] = $n;
-
-	// Делители m
-		
-	for($i = 1; $i <= ($m/2); $i++){
-
-		if($m%$i == 0){
-
-			$m_divisors[] = $i;
-		}
-	}
-
-	$m_divisors[] = $m;
-
-	// Поиск наибольшего общего делителя
-	
-	$n_count = count($n_divisors);
-
-	$m_count = count($m_divisors);
-	
 	$check = true;
-	
-	for($i = 0; $i < $n_count; $i++){
-		
-		for($j = 0; $j < $m_count; $j++){
-		
-			if($n_divisors[$i] != 1 && $n_divisors[$i] == $m_divisors[$j]){
 
-				$check = false;
-				
-				echo ("The numbers are coprime integers, they have at least one more common divisor: ".$n_divisors[$i]."<br>");
-				
-				break 2;
-			}
-		}
+	echo ("Числа: ".$n." и ".$m."<br>");
+
+	if($n < $m){
+
+		$temp = $n;
+		
+	}else{
+		
+		$temp = $m;
 	}
 
+	for($i = 2; $i <= ($temp); $i++){
+
+		if($n%$i == 0 && $m%$i == 0){
+
+			echo ("Числа не взаимно просты, они имеют по крайней мере еще один общий делитель: ".$i."<br>");
+							
+			$check = false;
+			
+			break;
+		}
+	}
+	
 	if($check){
 		
-		echo ("The numbers are not coprime integers, they have no common divisor: <br>");
+		echo ("Числа взаимно просты, у них отсутствуют общие делители кроме единицы <br>");
 	}
-	
-	var_dump($n_divisors);
-	
-	var_dump($m_divisors);
 
 ?>
