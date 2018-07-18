b1e8ea1ede7a31b24f96ba9a5049cfa51c4d7d77
dioge <cherkashn@gmail.com>
2018-07-18 16:33:24 +0300

    Update task_19.php

--- task_19.php
+++ task_19.php
@@ -6,32 +6,24 @@
 	
 	$m = rand();
 	
-	echo ("The numbers are ".$n." and ".$m.":<br>");
-	
-	// Оптимизация для более емкого подсчета
-	
 	if($n < $m){
 		
 		$lcm = $dividend = $m;
+		
 		$divisor = $n;
+		
 	}else{
 		
 		$lcm = $dividend = $n;
+		
 		$divisor = $m;
 	}
 	
-	// Подсчет
-	
-	for($i = 0;; $i++){
+	while(($lcm%$divisor) != 0){
 
-		if(($lcm%$divisor) == 0){
-			
-			echo ($lcm." is a least common multiple of ".$n." and ".$m."<br>");
-			
-			break;
-		}
-		
 		$lcm += $dividend;
 	}
+	
+	echo ("Число ".$lcm." является НОК ".$n." и ".$m."<br>");
 	
 ?>
