05f8e2a5ea6fc8f8928a2f42136fc1c466e14d84
dioge <cherkashn@gmail.com>
2018-07-18 16:46:14 +0300

    Update task_20.php

--- task_20.php
+++ task_20.php
@@ -4,50 +4,28 @@
 
 	$n = rand(1, 1000);
 	
-	echo ("The range is from 1 to ".$n.":<br>");
-	
-	// Находим все суммы
+	echo ("Диапазон от 1 до ".$n.":<br>");
 	
 	for($i = 1; $i <= $n; $i++){
 		
+		$sum = 0;
+
 		for($j = 1; $j <= ($i/2); $j++){
 
 			if($i%$j == 0){
 
-				$divisors[] = $j;
+				$sum += $j;
 			}
 		}
 
-		$divisors[] = $i;
-
-		$count = count($divisors);
-		
-		$sum = 0;
-		
-		for($j = 0; $j < $count; $j++){
-
-			$sum += $divisors[$j];
-		}
-		
-		$i_sums[$i] = $sum;
-		
-		unset($divisors);
-	}
-
-	// Высчитываем наибольшую сумму
-	
-	$biggest_sum = $number = 1;
-	
-	for($i = 1; $i <= $n; $i++){
-
-		if($i_sums[$i] > $biggest_sum){
+		if($biggest_sum < $sum){
 			
-			$biggest_sum = $i_sums[$i];
+			$biggest_sum = $sum;
 			
 			$number = $i;
 		}
 	}
-	
-	echo ($biggest_sum." is the biggest sum of ".$number." number in the range");
+
+	echo ($biggest_sum." является наибольшей суммой делителей числа ".$number." в диапазоне");
 
 ?>
