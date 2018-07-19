c10c0dd22874c87644508c7f9a440e9d00cd2d47
dioge <cherkashn@gmail.com>
2018-07-19 14:39:43 +0300

    Update task_24.php

--- task_24.php
+++ task_24.php
@@ -2,41 +2,127 @@
 
 <?php
 
-	$n = $m = mt_rand();
+	$n = mt_rand();
 	
-	// Двоичная система
+	echo ("Число в десятичной системе: <br>".$n);
 	
-	echo ("Decimal number is ".$n."<br>");
 	
-	while($n > 0){
-		
-		$n_bin = ($n%2).$n_bin;
+	echo ("<br>Число в двоичной системе: <br>");
 
-		$n = floor($n/2);
-	}
-	
-	echo ("Binary number is ".$n_bin."<br>");
-	
-	// Шестнадцатеричная система
-
-	$hex_array = array(10 => 'a', 11 => 'b', 12 => 'c', 13 => 'd', 14 => 'e', 15 => 'f');
-	
-	while($m > 0){
+	function get_binary_n($number){
 		
-		if(($m%16)>9){
+		$number = floor($number / 2);
+	
+		while($number > 0){
 			
-			$n_hex = $hex_array[($m%16)].$n_hex;
+			get_binary_n($number);
 			
-		}else{
-			
-			$n_hex = ($m%16).$n_hex;
+			echo($number % 2);
+
+			break;
 		}
+	}
+	
+	get_binary_n($n);
+
+	echo($n % 2);
+	
+	
+	
+	echo ("<br>Число в шестнадцатеричной системе: <br>0x");
+
+	function get_hexadecimal_n($number){
 		
-		$m = floor($m/16);
+		$number = floor($number/16);
+		
+		while($number > 0){
+			
+			get_hexadecimal_n($number);
+				
+			if(($number%16)>9){
+				
+				switch($number%16){
+					
+					case 10:
+					
+						echo ("a");
+						break;
+						
+					case 11:
+					
+						echo ("b");
+						break;
+						
+					case 12:
+					
+						echo ("c");
+						break;		
+						
+					case 13:
+					
+						echo ("d");
+						break;
+						
+					case 14:
+					
+						echo ("e");
+						break;
+						
+					case 15:
+					
+						echo ("f");
+						break;
+				}
+
+			}else{
+				
+				echo ($number%16);
+			}
+			
+			break;
+		}
+	}
+	
+	get_hexadecimal_n($n);
+	
+	if(($n%16)>9){
+				
+		switch($n%16){
+			
+			case 10:
+			
+				echo ("a");
+				break;
+				
+			case 11:
+			
+				echo ("b");
+				break;
+				
+			case 12:
+			
+				echo ("c");
+				break;		
+				
+			case 13:
+			
+				echo ("d");
+				break;
+				
+			case 14:
+			
+				echo ("e");
+				break;
+				
+			case 15:
+			
+				echo ("f");
+				break;
+		}
+
+	}else{
+		
+		echo ($n%16);
 	}
 
-	$n_hex = "0x".$n_hex;
-
-	echo ("Hexadecimal number is ".$n_hex);
-	
 ?>
