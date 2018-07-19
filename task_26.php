54238173f0ac7c237fcca3d83922c56d29c5f90b
dioge <cherkashn@gmail.com>
2018-07-19 15:23:47 +0300

    Update task_26.php

--- task_26.php
+++ task_26.php
@@ -1,50 +1,31 @@
 <pre>
 
 <?php
-	$n = rand(1, 10000);
+
+	$n = rand(1, 1000);
 	
-	echo ("The range is from 1 to ".$n.":<br>");
-	
-	// Находим все суммы
+	echo ("Диапазон от 1 до ".$n.":<br>");
 	
 	for($i = 1; $i <= $n; $i++){
-		
-		for($j = 1; $j <= ($i/2); $j++){
-            
-			if($i%$j == 0){
-                
-				$divisors[] = $j;
-			}
-		}
-		$divisors[] = $i;
-        
-		$count = count($divisors);
-        
 		
 		$sum = 0;
-		
-		for($j = 0; $j < $count; $j++){
-            
-			$sum += $divisors[$j];
+
+		for($j = 1; $j <= ($i/2); $j++){
+
+			if($i%$j == 0){
+
+				$sum += $j;
+			}
 		}
-		
-		$i_sums[$i] = $sum;
-		
-		unset($divisors);
-	}
-	// Высчитываем максимальню сумму
-	
-	$max_sum = $number = 1;
-	
-	for($i = 1; $i <= $n; $i++){
-        
-		if($i_sums[$i] > $max_sum){
+
+		if($biggest_sum < $sum){
 			
-			$max_sum = $i_sums[$i];
+			$biggest_sum = $sum;
 			
 			$number = $i;
 		}
 	}
-	
-	echo ($max_sum." is the max sum of ".$number." number in the range");
+
+	echo ($biggest_sum." является наибольшей суммой делителей числа ".$number." в диапазоне");
+
 ?>
