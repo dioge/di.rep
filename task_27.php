9a876db111e9c6efd7a76249f95b5663b6362170
dioge <cherkashn@gmail.com>
2018-07-19 16:10:36 +0300

    Update task_27.php

--- task_27.php
+++ task_27.php
@@ -2,59 +2,37 @@
 
 <?php
 	
-    $n = rand(1, 10000);
+    $n = rand(1, 10000); 
 	
-	echo ("The range is from 1 to ".$n.":<br>");
-	
-	// Находим все суммы простых делителей
+	echo ("Диапазон от 1 до ".$n.":<br>");
 	
 	for($i = 1; $i <= $n; $i++){
 		
-		for($j = 1; $j <= ($i/2); $j++){
+		$sum = 0;
+		
+		for($j = 2; $j <= ($i / 2); $j++){
             
-			if($i%$j == 0){
+			if($i % $j == 0){
                 
-                for($k = 2; $k <= ($j/2); $k++){
-
-                    if($j%$k == 0){
+                for($k = 2; $k <= ($j / 2); $k++){
+					
+                    if($j % $k == 0){
                         
                         continue 2;
                     }
                 }
-                
-				$divisors[] = $j;
+
+				$sum += $j;
 			}
 		}
 
-		$divisors[] = $i;
-        
-		$count = count($divisors);
-		
-		$sum = 0;
-		
-		for($j = 0; $j < $count; $j++){
-            
-			$sum += $divisors[$j];
-		}
-		
-		$i_sums[$i] = $sum;
-		
-		unset($divisors);
-	}
-    
-	// Высчитываем максимальню сумму простых делителей
-
-	$max_sum = $number = 1;
-	
-	for($i = 1; $i <= $n; $i++){
-        
-		if($i_sums[$i] > $max_sum){
+		if($sum > $max_sum){
 			
-			$max_sum = $i_sums[$i];
+			$max_sum = $sum;
 			
 			$number = $i;
 		}
 	}
-	
-	echo ($max_sum." is the max sum of ".$number." prime number in the range");
+
+	echo ($max_sum." является максимальной суммой простых делителей числа ".$number." в диапазоне");
 ?>
