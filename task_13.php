e6514f9b80cab098921a484b35921de20a87d1c9
dioge <cherkashn@gmail.com>
2018-07-23 16:45:40 +0300

    Update task_13.php

--- task_13.php
+++ task_13.php
@@ -9,44 +9,30 @@
 
 	$n = $control = $_GET["n"];
 
+	$count = 0;
+
 	for($i = $n; $i >= 1; $i /= 10){
 		
 		$count++;
 	}
 
-	if($count%2 != 0){
+	if($count % 2 != 0){
 
 		exit("Число ".$control." содержит нечетное количество цифр");
 	}
 
-	$half_count = 1;
+	$reverse = 0;
+		
+	while($n >= 1){
+		
+		$reverse = (($reverse*10) + $n%10);
+		
+		$n = floor($n/10);
+	}
+	
+	if($control != 0 && $control == $reverse){
+	
+		echo ("Число ".$control." является симметричным");
+	}
 
-	for($i = $count; $i > ($count/2); $i--){
-
-		$half_count *= 10;
-	}
-	
-	$half = $half_control = floor($n/$half_count);
-	
-	while($half >= 1){
-		
-		if($half%10 != $n%10){
-			
-			echo ("Число ".$half_control." не является ".$control." симметричным");
-		
-			$n = false;
-			
-			break;
-		}
-		
-		$half = floor($half/10); 
-		
-		$n = floor($n/10); 
-	}
-	
-	if($n){
-		
-		echo ("Число 2x".$half_control." является ".$control." симметричным");
-	}
-	
 ?>
