590f16af34593a3e277405ecb103f9aa7cca1453
dioge <cherkashn@gmail.com>
2018-07-23 09:21:45 +0300

    Update task_30.php

--- task_30.php
+++ task_30.php
@@ -1,8 +1,9 @@
 <form action="task_30.php" method="get">
 
 	<p>Введите натуральное число.</p>
+
 	<input type="text" name="n">
-	
+
 </form>
 
 <pre>
@@ -11,16 +12,13 @@
 
 	$n = $_GET["n"];
 
-	$natural_numbers_sequence = ((2*$n) - 1);
+	$sequence = ((2*$n) - 1);
 	
-	for($i = $n; $i <= $natural_numbers_sequence; $i++){
+	echo ("Числа-близнецы последовательности от ".$n." до ".$sequence."<br>");
 
-		$numbers_array[] = $i;
-	}
-	
 	$first_prime_number = $n;
 	
-	for($i = $n + 1; $i <= $natural_numbers_sequence; $i++){
+	for($i = $n + 1; $i <= $sequence; $i++){
 		
 		$check = true;
 				
@@ -29,6 +27,7 @@
 			if($i%$j == 0){
 
 				$check = false;
+				
 				continue 2;
 			}
 		}
@@ -39,14 +38,11 @@
 			
 			if(($last_prime_number - $first_prime_number) == 2){
 				
-				$twin_primes[] = $first_prime_number." and ".$last_prime_number;
-		
+				echo ($first_prime_number." и ".$last_prime_number."<br>");
 			}
 
 			$first_prime_number = $last_prime_number;
 		}
 	}
 
-	echo ("Числа-близнецы последовательности от ".$n." до ".$natural_numbers_sequence."<br>");
-	var_dump ($twin_primes);
 ?>
